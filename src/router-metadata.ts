6db240e77f254a9eca858a1f34691d11ba6b64fd
Fred Kleuver <fred@avurad.nl>
2018-02-18 05:39:33 +0100

    fix getModuleId

--- src/router-metadata.ts
+++ src/router-metadata.ts
@@ -75,7 +75,7 @@ export const routerMetadata: IRouterMetadataType = {
     PLATFORM.eachModule((key: string, val: any) => {
       if (typeof val === "object") {
         for (const name of Object.keys(val)) {
-          if (val[name] === target) {
+          if (val[name] === target || name === target.name) {
             moduleId = key;
 
             return true;
