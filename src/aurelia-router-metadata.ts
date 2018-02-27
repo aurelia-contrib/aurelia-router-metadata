4095857f0610500b2a558ae301b4b0892b8cc8f6
Fred Kleuver <fred@avurad.nl>
2018-02-27 01:54:43 +0100

    feat(configure): add au.use.plugin() support to configure the settings

--- src/aurelia-router-metadata.ts
+++ src/aurelia-router-metadata.ts
@@ -1,3 +1,18 @@
+import { Container } from "aurelia-dependency-injection";
+import { RouterMetadataConfiguration, RouterMetadataSettings } from "./aurelia-router-metadata";
+
+export function configure(fxconfig: any, configureSettings: (settings: RouterMetadataSettings) => void): void {
+  const settings = new RouterMetadataSettings();
+  if (typeof configureSettings === "function") {
+    configureSettings(settings);
+  }
+  const container = fxconfig.container as Container;
+  const config = new RouterMetadataConfiguration(container);
+  RouterMetadataConfiguration.INSTANCE = config;
+  container.registerInstance(RouterMetadataSettings, settings);
+  container.registerInstance(RouterMetadataConfiguration, config);
+}
+
 export * from "./interfaces";
 export * from "./decorators";
 export * from "./resource-loader";
