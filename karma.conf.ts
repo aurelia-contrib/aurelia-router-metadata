81914931b3e27604ed533f5c0705acc2dc3a819e
Fred Kleuver <fred@avurad.nl>
2018-03-10 03:40:13 +0100

    chore(test): improve test configurations, add vscode karma test debugging

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,27 +1,28 @@
-// tslint:disable:no-implicit-dependencies
-// tslint:disable:import-name
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import * as karma from "karma";
 import * as path from "path";
 import * as webpack from "webpack";
 
-export interface IConfig extends karma.Config, IConfigOptions {
-  set(config: IConfigOptions): void;
+export interface IKarmaConfig extends karma.Config, IKarmaConfigOptions {
+  transpileOnly?: boolean;
+  noInfo?: boolean;
+  coverage?: boolean;
+  tsconfig?: string;
+  devtool?: string;
+  set(config: IKarmaConfigOptions): void;
 }
 
-export interface IConfigOptions extends karma.ConfigOptions {
-  webpack: IConfiguration;
+export interface IKarmaConfigOptions extends karma.ConfigOptions {
+  webpack: webpack.Configuration;
   coverageIstanbulReporter: any;
   webpackServer: any;
+  customLaunchers: any;
 }
 
-export interface IConfiguration extends webpack.Configuration {
-  mode: "development" | "production";
-}
+export default (config: IKarmaConfig): void => {
+  const rules: webpack.Rule[] = [];
 
-export default (config: IConfig): void => {
-  config.set({
-    basePath: "./",
+  const options: IKarmaConfigOptions = {
+    basePath: config.basePath || "./",
     frameworks: ["jasmine"],
     files: ["test/setup.ts"],
     preprocessors: {
@@ -31,12 +32,9 @@ export default (config: IConfig): void => {
       mode: "development",
       resolve: {
         extensions: [".ts", ".js"],
-        modules: ["src", "node_modules"],
-        alias: {
-          bluebird: path.resolve(__dirname, "node_modules/bluebird/js/browser/bluebird.core")
-        }
+        modules: ["src", "node_modules"]
       },
-      devtool: "cheap-module-eval-source-map",
+      devtool: config.devtool || "cheap-module-eval-sourcemap",
       module: {
         rules: [
           {
@@ -44,52 +42,42 @@ export default (config: IConfig): void => {
             loader: "ts-loader",
             exclude: /node_modules/,
             options: {
-              configFile: path.resolve(__dirname, "configs/tsconfig-test.json"),
-              transpileOnly: !config.singleRun
+              configFile: config.tsconfig,
+              transpileOnly: config.transpileOnly
             }
-          },
-          {
-            test: /[\/\\]node_modules[\/\\]bluebird[\/\\].+\.js$/,
-            use: [{ loader: "expose-loader?Promise" }]
-          },
-          {
-            enforce: "post",
-            exclude: /(node_modules|\.spec\.ts$)/,
-            loader: "istanbul-instrumenter-loader",
-            options: { esModules: true },
-            test: /src[\/\\].+\.ts$/
           }
         ]
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          Promise: "bluebird"
-        }),
-        ...(config.singleRun
-          ? [new webpack.NoEmitOnErrorsPlugin()]
-          : [
-              new ForkTsCheckerWebpackPlugin({
-                watch: ["./src"],
-                formatter: "codeframe",
-                tsconfig: path.resolve(__dirname, "configs/tsconfig-test.json")
-              })
-            ])
-      ]
+      }
     },
     mime: {
       "text/x-typescript": ["ts"]
     },
-    reporters: ["mocha", "progress", "coverage-istanbul"],
-    coverageIstanbulReporter: {
+    reporters: ["mocha", "progress"],
+    webpackServer: { noInfo: config.noInfo },
+    browsers: config.browsers || ["Chrome"],
+    customLaunchers: {
+      ChromeDebugging: {
+        base: "Chrome",
+        flags: ["--remote-debugging-port=9333"],
+        debug: true
+      }
+    }
+  };
+
+  if (config.coverage) {
+    options.webpack.module.rules.push({
+      enforce: "post",
+      exclude: /(node_modules|\.spec\.ts$)/,
+      loader: "istanbul-instrumenter-loader",
+      options: { esModules: true },
+      test: /src[\/\\].+\.ts$/
+    });
+    options.reporters.push("coverage-istanbul");
+    options.coverageIstanbulReporter = {
       reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
-    },
-    webpackServer: { noInfo: true },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["ChromeHeadless"],
-    singleRun: false
-  });
+    };
+  }
+
+  config.set(options);
 };
--- package-scripts.js
+++ package-scripts.js
@@ -5,11 +5,11 @@ function config(name) {
 }
 
 function tslint(tsconfig) {
-  return crossEnv(`tslint --project ${config(tsconfig)}`);
+  return package(`tslint --project ${config(tsconfig)}`);
 }
 
 function tsc(tsconfig) {
-  return crossEnv(`tsc --project ${config(tsconfig)}`);
+  return package(`tsc --project ${config(tsconfig)}`);
 }
 
 function bumpVersion(version) {
@@ -17,34 +17,49 @@ function bumpVersion(version) {
 }
 
 function webpack(tool, arg) {
-  return crossEnv(`TS_NODE_ENV=\"${config("webpack")}\" ${tool} --config webpack.config.ts ${arg}`)
+  return crossEnv(`TS_NODE_ENV=\"${config("webpack")}\" ${tool} --config webpack.config.ts ${arg}`);
+}
+
+function package(script) {
+  return crossEnv(`./node_modules/.bin/${script}`);
+}
+
+function karma(single, watch, browsers, transpileOnly, noInfo, coverage, tsconfig, logLevel, devtool) {
+  return package(
+    "karma start"
+      .concat(single !== null ? ` --single-run=${single}` : "")
+      .concat(watch !== null ? ` --auto-watch=${watch}` : "")
+      .concat(browsers !== null ? ` --browsers=${browsers}` : "")
+      .concat(transpileOnly !== null ? ` --transpile-only=${transpileOnly}` : "")
+      .concat(noInfo !== null ? ` --no-info=${noInfo}` : "")
+      .concat(coverage !== null ? ` --coverage=${coverage}` : "")
+      .concat(tsconfig !== null ? ` --tsconfig=${tsconfig}` : "")
+      .concat(logLevel !== null ? ` --log-level=${logLevel}` : "")
+      .concat(devtool !== null ? ` --devtool=${devtool}` : "")
+  );
 }
 
 function release(version) {
   return {
-    default: series.nps(
-      `release.${version}.before`,
-      `release.version`,
-      `release.${version}.after`
-    ),
-    before: series.nps(
-      `release.build`,
-      `release.${version}.bump`,
-      `release.git.stage`
-    ),
-    after: series.nps(
-      `release.git.push`,
-      `release.npm.publish`,
-    ),
-    bump: bumpVersion(version),
-  }
+    default: series.nps(`release.${version}.before`, `release.version`, `release.${version}.after`),
+    before: series.nps(`release.build`, `release.${version}.bump`, `release.git.stage`),
+    after: series.nps(`release.git.push`, `release.npm.publish`),
+    bump: bumpVersion(version)
+  };
 }
 
 module.exports = {
   scripts: {
     lint: tslint("build"),
-    test: "karma start --single-run",
-    dev: "karma start",
+    test: {
+      default: "nps test.single",
+      single: karma(true, false, "ChromeHeadless", true, true, true, config("test"), null, null),
+      watch: {
+        default: "nps test.watch.dev",
+        dev: karma(false, true, "ChromeHeadless", true, true, true, config("test"), null, null),
+        debug: karma(false, true, "ChromeDebugging", true, false, null, config("test"), "debug", null)
+      }
+    },
     build: {
       demos: {
         default: "nps build.demos.development",
@@ -56,14 +71,8 @@ module.exports = {
         }
       },
       dist: {
-        default: series.nps(
-          "build.dist.before",
-          "build.dist.all"
-        ),
-        before: series.nps(
-          "lint",
-          "build.dist.clean"
-        ),
+        default: series.nps("build.dist.before", "build.dist.all"),
+        before: series.nps("lint", "build.dist.clean"),
         all: concurrent.nps(
           "build.dist.amd",
           "build.dist.commonjs",
@@ -86,10 +95,7 @@ module.exports = {
       minor: release("minor"),
       major: release("major"),
       version: "standard-version --first-release --commit-all",
-      build: series.nps(
-        "test",
-        "build.dist"
-      ),
+      build: series.nps("test", "build.dist"),
       git: {
         stage: "git add package.json dist",
         push: "git push --follow-tags origin master"
@@ -104,7 +110,7 @@ module.exports = {
       rimraf("*.bundle.js"),
       "nps build.demos.production",
       "git add index.html *.bundle.js",
-      "git commit -m \"doc(demos): build demos\"",
+      'git commit -m "doc(demos): build demos"',
       "git push",
       "git checkout master"
     )
